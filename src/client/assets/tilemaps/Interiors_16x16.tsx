7896b2281599bdb5d2dd490637e2a90915598cd7
Greg Tatum <tatum.creative@gmail.com>
2020-11-29 21:30:18 -0600

    Add some new tilemap

---
+++ src/client/assets/tilemaps/Interiors_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="Interiors_16x16" tilewidth="16" tileheight="16" tilecount="5200" columns="16">
+ <image source="../Modern_Interiors/1_Tilesets/16x16/Interiors_16x16.png" width="256" height="5200"/>
+</tileset>
